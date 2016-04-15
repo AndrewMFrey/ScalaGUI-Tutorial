a000a1fc6e304ba42ad87b1eea593a39cba56362
Andrew Frey <andrewmarkfrey@gmail.com>
2016-04-14 22:25:46 -0700

    Initial Commit

---
+++ source/gui3.scala
@@ -0,0 +1,25 @@
+import scala.swing._
+
+class UI extends MainFrame {
+  title = "GUI Program #3"
+  contents = new BoxPanel(Orientation.Vertical) {
+    contents += new Label("Look at me!")
+    contents += Swing.VStrut(10)
+    contents += Swing.Glue
+    contents += Button("Press me, please") {
+      println("Thank you")
+    }
+    contents += Button("Close") {
+      sys.exit(0)
+    }
+    border = Swing.CompoundBorder(Swing.BeveledBorder(Swing.Lowered),
+                                  Swing.EmptyBorder(10))
+  }
+}
+
+object GuiProgramThree {
+  def main(args: Array[String]) {
+    val ui = new UI
+    ui.visible = true
+  }
+}
